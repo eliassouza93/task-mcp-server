05ef15f27997accb0da2230cc7800b8649f30e92
eliassouza93 <elias99-souza@hotmail.com>
2026-01-06 16:41:02 -0300

    fix: tool add sumay

--- src/mcp/server.ts
+++ src/mcp/server.ts
@@ -3,9 +3,14 @@ import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
 
 import { z } from "zod";
 
-async function main() {
-  console.log("🚀 MCP Server iniciado");
+type Task = {
+  id: string;
+  title: string;
+  description?: string;
+  completed: boolean;
+};
 
+async function main() {
   const server = new McpServer({
     name: "task-mcp",
     version: "1.0.0",
@@ -24,7 +29,7 @@ async function main() {
   //aqui chama a api com fetch
   server.tool(
     "get_task_by_id",
-    "Busca uma tarefa pelo ID",
+    "busca uma tarefa pelo ID",
     {
       id: z.string().describe("UUID da tarefa"),
     },
@@ -40,6 +45,48 @@ async function main() {
           },
         ],
       };
+    }
+  );
+  //busca e retorna um resumo das tarefas concluidas ou pendentes
+  server.tool(
+    "task_summary",
+    "retorna um resumo das tarefas (total, concluídas e pendentes)",
+    {},
+    async () => {
+      try {
+        const res = await fetch("http://localhost:3000/");
+        const tasks = (await res.json()) as Task[];
+
+        const total = tasks.length;
+        const completed = tasks.filter((t: any) => t.completed).length;
+        const pending = total - completed;
+
+        return {
+          content: [
+            {
+              type: "text",
+              text: JSON.stringify(
+                {
+                  total,
+                  completed,
+                  pending,
+                },
+                null,
+                2
+              ),
+            },
+          ],
+        };
+      } catch (error: any) {
+        return {
+          content: [
+            {
+              type: "text",
+              text: `erro ao gerar resumo: ${error.message}`,
+            },
+          ],
+        };
+      }
     }
   );
 
